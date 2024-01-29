264f383fbc0f7c26709b70fb96eff7b0c30d4c42
Tin Tun Naing <ttnaing@nevion.com>
2024-01-29 20:07:19 +0100

    MSE graph
    Co-authored-by: Mortaza Baqeri  <mortazaBaqeri@users.noreply.github.com>

--- ControlSystem.py
+++ ControlSystem.py
@@ -1,5 +1,6 @@
 import numpy as np
-
+import jax.numpy as jnp 
+import matplotlib.pyplot as plt
 
 def disturbance_generator(x1=-1, x2=1):
     """
@@ -18,6 +19,7 @@ class ControlSystem:
         self.plant = plant
 
     def run_control_loop(self, setpoint, epochs, timesteps):
+        mse_history = []
         for epoch in range(epochs):
             error_history = []
             for timestep in range(timesteps):
@@ -36,3 +38,25 @@ class ControlSystem:
                 self.controller.update(setpoint, self.plant.state, timestep)
 
             mse = np.mean(np.square(np.array(error_history)))
+            mse_history.append(mse)
+
+        self.plot(mse_history)
+
+    def plot(self, mse_history):
+        mse_np = jnp.array(mse_history)
+
+        # Visualization
+        plt.figure(figsize=(12, 6))
+
+        # Plotting mean squared error
+        plt.subplot(1, 2, 1)
+        plt.plot(mse_np)
+        plt.xlabel('Time Steps')
+        plt.axvline(x=10, color='red', linestyle='--')  # Mark x=10
+        plt.ylabel('Mean Squared Error')
+        plt.title('Mean Squared Error Over Time')
+        plt.ylim(0, 1)
+        plt.xlim(0, 100)
+
+        plt.tight_layout()
+        plt.show()
