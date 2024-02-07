a772a660d21947112aa6db0c7500bcbea246f94a
Mortaza Baqeri <mortazab@stud.ntnu.no>
2024-02-07 21:40:13 +0100

    code nbote workin 100%

--- xxx.py
+++ xxx.py
@@ -5,15 +5,14 @@ from bathtub import bathtub
 
 
 # Feed forward neural network with 5 inputs, 10 hidden nodes, and 5 outputs with JAX
-
 class NNController:
-    def __init__(self, sizes, key):
+    def __init__(self, sizes):
         self.layer_sizes = sizes
 
     def gen_jaxnet_params(self, layers=[3, 10, 1]):
         sender = layers[0]
         params = []
-        # one set of weights and bases for each non-input layer
+        # one set of weights and biases for each non-input layer
         for receiver in layers[1:]:
             weights = np.random.uniform(-.1, .1, (sender, receiver))  # Using numpy because this is not traced by jax
             biases = np.random.uniform(-.1, .1, (1, receiver))
@@ -21,21 +20,23 @@ class NNController:
             params.append([weights, biases])
         return params
 
+    def sigmoid(self, x):
+        """Activation function"""
+        return 1 / (1 + jnp.exp(-x))
 
     def predict(self, all_params, features):
         # nn_forward
-        """Returned activations = the net's output"""
+        """
+        :param all_params: weights and biases
+        :param features: input features, (PID) for the bathtub model
+        Returned activations = the net's output"""
+
         activations = features
         # Feed the features forward through all layers of the net
         for weights, biases in all_params:
             activations = self.sigmoid(jnp.dot(activations, weights) + biases)
 
         return activations
-
-    def sigmoid(self, x):
-        """Activation function"""
-        return 1 / (1 + jnp.exp(-x))
-
 
     def jaxnet_loss(self, params, features, targets):
         # Make a batched version of the `predict` function.
@@ -46,70 +47,86 @@ class NNController:
         print(f"Targets: {targets} and predictions: {predictions}")
         return jnp.mean(jnp.square(predictions - targets))
 
+    #def jaxnet_train_one_epoch(self, params, features, targets, lrate=0.1):
+    def jaxnet_train_one_epoch(self, params, lrate=0.03):
+        """ basically the update findtion i think !
+        Mean squared error and gradient function with JAX tracing
+        """
 
+        plant = bathtub(A=100, C=0.01, H_0=100)
+        num_timesteps = 100
+        total_loss = 0.0
 
+        for i in range(num_timesteps):
+            noise = disturbance_generator()
 
+            current_state = plant.state
+            pid_err = pid_error_controller.calculate_errors(current_state)
 
+            # TODO ta den ut av self kalssen, basically gjør det modulært!!!!!!!!
+            control_signal = self.predict(params, pid_err)
 
-def jaxnet_train_one_epoch(params, features, targets, lrate=0.1):
-    # Mean squared error and gradient function with JAX tracing
-    # Alternative: jax.grad(jaxnet_loss)(params, features,targets)
-    mse, gradients = jax.value_and_grad(jaxnet_loss)(params, features, targets)
-    return [(w - lrate * dw, b - lrate * db) for (w, b), (dw, db) in zip(params, gradients)], mse
+            # plant.update_state(control_signal, D)
+            plant.update(control_signal, noise)
+
+            error = 100 - current_state
+            total_loss += error ** 2
+
+        mse = total_loss / num_timesteps
+        return (jnp.squeeze(mse))
+
+    #def jaxnet_train(self, params, features, targets, epochs, lrate=0.1):
+    def jaxnet_train(self, params, epochs, lrate=0.1):
+        error_history = []
+        curr_params = params
+        for epoch in range(epochs):
+            #curr_params, mse = self.jaxnet_train_one_epoch(curr_params, features, targets, lrate)
+            curr_params, mse = self.jaxnet_train_one_epoch(curr_params, lrate)
+
+            error_history.append(mse)
+
+        return curr_params
+
+    def jaxrun(self, epochs, layer_sizes, lrate=0.03):
+        # features, targets = generate_data_cases(ncases)
+        # features should be PID
+        params = self.gen_jaxnet_params(layer_sizes)
+
+        # TODO: gjør setpoint modulært
+
+        targets = 100
+
+        #self.jaxnet_train(params, features, targets, epochs, lrate=lrate)
 
 
 def disturbance_generator(x1=-0.01, x2=0.01):
     return np.random.uniform(x1, x2)
 
+class PIDErrorCalculator:
+    def __init__(self, set_point):
+        self.set_point = set_point
+        self.integral_error = 0
+        self.previous_error = 0
 
-def jaxnet_train(params, features, targets, epochs, lrate=0.1):
-    error_history = []
-    control_signal_history = []
-    print("targets: ", targets)
-    current_parameters = params
+    def reset(self):
+        self.integral_error = 0
+        self.previous_error = 0
 
-    for epoch in range(epochs):
-        plant = bathtub(A=10, C=.1, H_0=100)  # resetting the plant
-        current_level = plant.get_error()
+    def calculate_errors(self, current_value):
+        error = self.set_point - current_value
+        self.integral_error += error
+        derivative_error = error - self.previous_error
+        self.previous_error = error
 
-        # BUG: tror dette er feil!!!!
-        error = targets - current_level
-        error_arrary = jnp.array([error]).reshape(1, -1)
-        control_signal = predict(params, error_arrary)
-        control_signal_history.append(control_signal)
-
-        noise = disturbance_generator()
-        # plant.update(control_signal[0], noise) # Prøv dette hvis første ikke funker
-        plant.update(control_signal, noise)
-
-        error_history.append(plant.get_error())
-
-    curr_params, mse = jaxnet_train_one_epoch(curr_params, features, targets, lrate)
-    print(mse)
-    return curr_params
+        return [error, self.integral_error, derivative_error]
 
 
-def generate_data_cases(ncases=1000):
-    features = np.random.uniform(-1, 1, (ncases, 5))
-    targets = np.random.uniform(-1, 1, (ncases, 5))
-    return features, targets
-
-
-def jaxrun(epochs, ncases, layer_sizes, lrate=0.03):
-    # features, targets = generate_data_cases(ncases)
-    # features should be PID
-
-
-
-    params = gen_jaxnet_params(layer_sizes)
-    jaxnet_train(params, features, targets, epochs, lrate=lrate)
-
+pid_error_controller = PIDErrorCalculator(set_point=100)
 
 if __name__ == "__main__":
     epochs = 100
-    ncases = 1000
     lrate = 0.03
     layer_sizes = [3, 10, 1]
-    jaxrun(epochs, ncases, layer_sizes, lrate=lrate)
 
-
+    nn = NNController(layer_sizes)
+    nn.jaxrun(epochs, layer_sizes, lrate=lrate)
