c2343897d858785b0a7bb3bc9ea5fecc372acce6
Mortaza Baqeri <mortazab@stud.ntnu.no>
2024-02-08 17:12:59 +0100

    JAØLSDJLKJASDJ 22222

--- PID_run.py
+++ PID_run.py
@@ -1,10 +1,8 @@
+import numpy as np
 import matplotlib.pyplot as plt
+from bathtub import bathtub
 import jax.numpy as jnp
 import jax
-
-from plants import BathtubPlant
-from utils import disturbance_generator
-
 
 class PID:
 
@@ -37,30 +35,31 @@ class PID:
 
 
 # TODO: Mortaza: skal endres!
-def make_loss_function(time_sample, plant):
+def make_loss_function(setpoint, time_sample):
     def loss_fn(kp, ki, kd):
-        return calculate_mse(kp, ki, kd, time_sample, plant)
+        return calculate_mse(kp, ki, kd, setpoint, time_sample)
     return loss_fn
 
-def calculate_mse(kp, ki, kd, time_sample, plant):
+def calculate_mse(kp, ki, kd, setpoint, time_sample):
     # TODO: fikse dt på en bedre måte
     controller = PID(kp, ki, kd, 1)
+    plant = bathtub(A=10, C=0.1, H_0=setpoint)
     error_history_over_one_epoch = []
-    print(plant.get_error())
 
     for step in range(time_sample):
-
         noise = disturbance_generator(x1=-0.01, x2=0.01)  # Noise
         error = plant.get_error()
         controller_output = controller.update(error)
-        plant.update_state(controller_output, noise)
+        plant.update(U=controller_output, D=noise)
         error_history_over_one_epoch.append(error)
 
-    print(error_history_over_one_epoch)
     squared_errors = jnp.square(jnp.array(error_history_over_one_epoch))
     mse = jnp.mean(squared_errors)
     print("mean sqaure error: ", mse)
     return mse
+
+def disturbance_generator(x1=-0.01, x2=0.01):
+    return np.random.uniform(x1, x2)
 
 
 def run_program():
@@ -73,10 +72,8 @@ def run_program():
     # bathtub
     setpoint = 100  # aka. Initial_height
 
-    plant = BathtubPlant(setpoint, 10, 0.01)
-
     # Prepare the loss function for gradients
-    loss_fn = make_loss_function(num_time_steps, plant)
+    loss_fn = make_loss_function(setpoint, num_time_steps)
 
     # Compute gradients of the loss function w.r.t. PID parameters
     grad_loss_fn = jax.grad(loss_fn, argnums=(0, 1, 2))
@@ -89,17 +86,12 @@ def run_program():
 
     for epoch in range(num_epochs):
         # Compute gradients
-
-        print(f"pid_params ${pid_params}")
         grads = grad_jit(pid_params['kp'], pid_params['ki'], pid_params['kd'])
-
 
         # Update PID parameters
         pid_params['kp'] -= learning_rate * grads[0]
         pid_params['ki'] -= learning_rate * grads[1]
         pid_params['kd'] -= learning_rate * grads[2]
-
-        print(f"pid_params 2 ${pid_params['kp']} and ${pid_params['ki']} and ${pid_params['kd']}")
 
         # Store parameter values
         kp_history.append(pid_params['kp'])
@@ -108,7 +100,6 @@ def run_program():
 
         # Calculate and store the MSE for this epoch
         mse = loss_fn(pid_params['kp'], pid_params['ki'], pid_params['kd'])
-        print(f"mse: {mse}")
         mse_history.append(mse)
 
     # convert to jnp.array
